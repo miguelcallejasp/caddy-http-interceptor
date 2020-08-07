38f61397cd43e6e2b24430873829192162465dfb
miguelcallejasp <miguelcallejasp@gmail.com>
2020-08-07 15:59:34 -0400

    adding debug line

--- run.sh
+++ run.sh
@@ -8,11 +8,21 @@ echo "Listening on port: ${LISTENING_PORT}"
 echo "Destination address: ${DESTINATION_ADDRESS}"
 echo "Destination port: ${DESTINATION_PORT}"
 echo "Component tag: ${POD_TAG}"
+echo "Debug Enabled: ${DEBUG}"
 
 sed -i "s|listening_port|${LISTENING_PORT}|g" /etc/Caddyfile
 sed -i "s|destination|${DESTINATION_ADDRESS}|g" /etc/Caddyfile
 sed -i "s|port|${DESTINATION_PORT}|g" /etc/Caddyfile
 sed -i "s|podtag|${POD_TAG}|g" /etc/Caddyfile
+
+if [[ $DEBUG == "true" ]]
+then
+    #line 4 has the debug log
+    sed '4s/^/#/' /etc/Caddyfile 
+else
+    #line 3 has the debug log
+    sed '3s/^/#/' /etc/Caddyfile
+fi
 
 echo "Starting Proxy"
 /usr/bin/caddy --conf /etc/Caddyfile --http2=false
